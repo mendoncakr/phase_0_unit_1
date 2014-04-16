48f4415a3b280e814a51d0dfbaeacc46a8179c46
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-04-16 18:37:12 -0400

    added user story for group project

---
+++ week_3/5_group_project/gc1_user_stories.js
@@ -0,0 +1,13 @@
+// Kenneth Mendonça
+
+// I want three things that will calculate the following for me if I have a group 
+// of numbers in a set. Theres no particular order to this set, just random numbers.
+// I want the first thing to be called  "sum" and I want it to  print out the sum of all the numbers 
+// in the set. The sum is  defined as the addition of all numbers contained within that set.
+// Next, I want the second thing to be called "mean" and it should give me the average of the set.
+// By average I mean, the sum divided by the # of items in the set. Finally, I want my third thing to be called "median" 
+// and it should give me the middle number within the set. If my set of numbers is even it should give me the average 
+// of the two middle numbers. On the other hand, the set contains an odd amount of numbers, it should 
+// only give me the middle one.
+
+
