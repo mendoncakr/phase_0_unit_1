df5593170e90ed5106ac900598066adf3feedd1b
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-04-19 17:15:45 -0400

    completed solutions

--- week_3/2_js_variables_objects/my_solution.js
+++ week_3/2_js_variables_objects/my_solution.js
@@ -1,40 +1,35 @@
-// I paired [by myself, with:] on this challenge.
+// I paired with Scott Jason on this challenge.
 
 
 
 
 // Pseudocode
-// 
-// 
-// 
-// 
+// create secretNumber variable and set to 7
+// create password variable and set to string
+// create allowedin and set false
+// create members array 
 
 
 // __________________________________________
 // Write your code below.
 var secretNumber = 7;
-
-
-
-
+var password = "just open the door";
+var allowedIn = false;
+var members = ["John", "Kenneth", "Noni", "Mary"];
 
 
 // __________________________________________
 // Refactored Code: Include a refactored version (or justification of your original code) here. 
-
+// There is no way to refactor this code
 
 
 
 
 // __________________________________________
 // Reflection: Use the reflection guidelines to write a reflection here. 
+// This was almost too easy to do and I felt as if I didn't learn anything much of new as 
+// I pretty much only had to declare 4 variables.
 // 
-// 
-// 
-// 
-// 
-// 
-
 
 // __________________________________________
 // Driver Code:  Do not alter code below this line.
--- week_3/8_gps1_2/our_solution.js
+++ week_3/8_gps1_2/our_solution.js
@@ -1,8 +1,8 @@
 // GPS 1.2 - JavaScript
 
 // YOUR FULL NAMES:
-//  1.
-//  2.
+//  1. Kenneth Mendonca	
+//  2. Ian Shuff
 
 
 // 1. "YOU SIGNED... WHO?!"
@@ -38,3 +38,28 @@ shooterMcGavin.quote === "Just stay out of my way... or you'll pay. Listen to wh
 
 
 //  Your Reflection:
+
+var client1 = {name: "Ian Shuff", age: 25, quote: "Hey what's up"};
+
+var client2 = {name: "Adam Sandler", age: 47, quote: "That's your home, are you too good for home?"};
+var client3 = {name: "Kristen Bell", age: 33, quote: "Do you wanna build a snowman?"};
+var client4 = {name: "Jim Carrey", age: 52, quote: "So you're telling me there's a chance? Yeah!"};
+
+var clientlist = [client1, client2, client3, client4];
+
+function Client(name, age, quote) {
+this.name = name;
+this.age = age;
+this.quote = quote;
+console.log("My name is " + array[i].name + " and I'm " + array[i].age.toString() +" years old." +" My best quote is: " + array[i].quote) ;
+}
+
+client5 = new Client("Ben Affleck",44,"Boston!");
+client6 = new Client("Jennifer Lawrence", 25, "YEAH!");
+client7 = new Client("Henry Cavill", 27, "Up, up, and away.");
+
+array = [];
+array.push(client5,client6, client7);
+
+for ( var i=0; i < array.length; i++ ) {
+
